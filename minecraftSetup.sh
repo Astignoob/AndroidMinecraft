f19109e4df5ee57251dda7f42d46885dcd6222de
Astignoob <65780570+Astignoob@users.noreply.github.com>
2021-08-11 21:52:43 +0800

    Update minecraftSetup.sh

--- minecraftSetup.sh
+++ minecraftSetup.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 apt-get update
-apt install software-properties-common
-add-apt-repository ppa:openjdk-r/ppa
-apt-get install openjdk-8-jre nano zip unzip
-mkdir mc
-cd mc
-wget -O minecraft_server.jar https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
+wget -O server1.17.1.jar https://download.getbukkit.org/spigot/spigot-1.17.1.jar
 echo "eula=true" > eula.txt
 cd ..
 wget -O ngrok.zip https://bin.equinox.io/a/nmkK3DkqZEB/ngrok-2.2.8-linux-arm64.zip
 unzip ngrok.zip
-printf "cd mc\njava -Xmx1G -jar minecraft_server.jar nogui" > m
-printf "cd ngrok\n./ngrok tcp 25565" > n
+printf "cd mc\njava -Xmx1G -jar server1.17.1.jar nogui" > m
+printf "cd ngrok\n./ngrok tcp -region=ap 25565" > n
 chmod +x m
 chmod +x n
